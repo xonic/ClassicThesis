8268731a7018cc7661a2a8919a7c97cfdec5f93e
Thomas Nägele <xonic@gmx.eu>
2011-02-19 15:54:26 +0100

    add classicthesis.ex

--- ClassicThesis.ex
+++ ClassicThesis.ex
@@ -1,35 +1,35 @@
-\contentsline {captionX}{ {Auzug 1}\hspace *{1cm}Vorschlag von Kerry.}{57}{captionX.1}
-\contentsline {captionX}{ {Auzug 2}\hspace *{1cm}Johns Sicherstellung.}{58}{captionX.2}
-\contentsline {captionX}{ {Auzug 3}\hspace *{1cm}Kerrys Hinweis.}{58}{captionX.3}
-\contentsline {captionX}{ {Auzug 4}\hspace *{1cm}Kerrys Anliegen wird ignoriert.}{58}{captionX.4}
-\contentsline {captionX}{ {Auzug 5}\hspace *{1cm}Kerry hat Erfolg.}{59}{captionX.5}
-\contentsline {captionX}{ {Auzug 6}\hspace *{1cm}Johns Vorschlag.}{59}{captionX.6}
-\contentsline {captionX}{ {Auzug 7}\hspace *{1cm}Kerry best\IeC {\"a}tigt Ivan.}{59}{captionX.7}
-\contentsline {captionX}{ {Auzug 8}\hspace *{1cm}.}{61}{captionX.8}
-\contentsline {captionX}{ {Auzug 9}\hspace *{1cm}.}{61}{captionX.9}
-\contentsline {captionX}{ {Auzug 10}\hspace *{1cm}.}{62}{captionX.10}
-\contentsline {captionX}{ {Auzug 11}\hspace *{1cm}.}{62}{captionX.11}
-\contentsline {captionX}{ {Auzug 12}\hspace *{1cm}.}{62}{captionX.12}
-\contentsline {captionX}{ {Auzug 13}\hspace *{1cm}.}{63}{captionX.13}
-\contentsline {captionX}{ {Auzug 14}\hspace *{1cm}.}{63}{captionX.14}
-\contentsline {captionX}{ {Auzug 15}\hspace *{1cm}.}{63}{captionX.15}
-\contentsline {captionX}{ {Auzug 16}\hspace *{1cm}.}{64}{captionX.16}
-\contentsline {captionX}{ {Auzug 17}\hspace *{1cm}.}{64}{captionX.17}
-\contentsline {captionX}{ {Auzug 18}\hspace *{1cm}.}{65}{captionX.18}
-\contentsline {captionX}{ {Auzug 19}\hspace *{1cm}.}{66}{captionX.19}
-\contentsline {captionX}{ {Auzug 20}\hspace *{1cm}.}{66}{captionX.20}
-\contentsline {captionX}{ {Auzug 21}\hspace *{1cm}.}{67}{captionX.21}
-\contentsline {captionX}{ {Auzug 22}\hspace *{1cm}.}{67}{captionX.22}
-\contentsline {captionX}{ {Auzug 23}\hspace *{1cm}.}{68}{captionX.23}
-\contentsline {captionX}{ {Auzug 24}\hspace *{1cm}.}{69}{captionX.24}
-\contentsline {captionX}{ {Auzug 25}\hspace *{1cm}.}{70}{captionX.25}
-\contentsline {captionX}{ {Auzug 26}\hspace *{1cm}.}{71}{captionX.26}
-\contentsline {captionX}{ {Auzug 27}\hspace *{1cm}.}{72}{captionX.27}
-\contentsline {captionX}{ {Auzug 28}\hspace *{1cm}.}{72}{captionX.28}
-\contentsline {captionX}{ {Auzug 29}\hspace *{1cm}.}{72}{captionX.29}
-\contentsline {captionX}{ {Auzug 30}\hspace *{1cm}.}{72}{captionX.30}
-\contentsline {captionX}{ {Auzug 31}\hspace *{1cm}.}{73}{captionX.31}
-\contentsline {captionX}{ {Auzug 32}\hspace *{1cm}.}{74}{captionX.32}
-\contentsline {captionX}{ {Auzug 33}\hspace *{1cm}.}{75}{captionX.33}
-\contentsline {captionX}{ {Auzug 34}\hspace *{1cm}Gestik in Designmeetings.}{96}{captionX.34}
-\contentsline {captionX}{ {Auzug 35}\hspace *{1cm}Arbeiten an digitalen Tabletop-Systemen.}{102}{captionX.35}
+\contentsline {captionX}{ {Auzug 1}\hspace *{1cm}Vorschlag von Kerry.}{55}{captionX.1}
+\contentsline {captionX}{ {Auzug 2}\hspace *{1cm}Johns Sicherstellung.}{55}{captionX.2}
+\contentsline {captionX}{ {Auzug 3}\hspace *{1cm}Kerrys Hinweis.}{56}{captionX.3}
+\contentsline {captionX}{ {Auzug 4}\hspace *{1cm}Kerrys Anliegen wird ignoriert.}{56}{captionX.4}
+\contentsline {captionX}{ {Auzug 5}\hspace *{1cm}Kerry hat Erfolg.}{56}{captionX.5}
+\contentsline {captionX}{ {Auzug 6}\hspace *{1cm}Johns Vorschlag.}{57}{captionX.6}
+\contentsline {captionX}{ {Auzug 7}\hspace *{1cm}Kerry best\IeC {\"a}tigt Ivan.}{57}{captionX.7}
+\contentsline {captionX}{ {Auzug 8}\hspace *{1cm}.}{58}{captionX.8}
+\contentsline {captionX}{ {Auzug 9}\hspace *{1cm}.}{59}{captionX.9}
+\contentsline {captionX}{ {Auzug 10}\hspace *{1cm}.}{59}{captionX.10}
+\contentsline {captionX}{ {Auzug 11}\hspace *{1cm}.}{60}{captionX.11}
+\contentsline {captionX}{ {Auzug 12}\hspace *{1cm}.}{60}{captionX.12}
+\contentsline {captionX}{ {Auzug 13}\hspace *{1cm}.}{60}{captionX.13}
+\contentsline {captionX}{ {Auzug 14}\hspace *{1cm}.}{61}{captionX.14}
+\contentsline {captionX}{ {Auzug 15}\hspace *{1cm}.}{61}{captionX.15}
+\contentsline {captionX}{ {Auzug 16}\hspace *{1cm}.}{61}{captionX.16}
+\contentsline {captionX}{ {Auzug 17}\hspace *{1cm}.}{62}{captionX.17}
+\contentsline {captionX}{ {Auzug 18}\hspace *{1cm}.}{62}{captionX.18}
+\contentsline {captionX}{ {Auzug 19}\hspace *{1cm}.}{63}{captionX.19}
+\contentsline {captionX}{ {Auzug 20}\hspace *{1cm}.}{64}{captionX.20}
+\contentsline {captionX}{ {Auzug 21}\hspace *{1cm}.}{64}{captionX.21}
+\contentsline {captionX}{ {Auzug 22}\hspace *{1cm}.}{65}{captionX.22}
+\contentsline {captionX}{ {Auzug 23}\hspace *{1cm}.}{66}{captionX.23}
+\contentsline {captionX}{ {Auzug 24}\hspace *{1cm}.}{67}{captionX.24}
+\contentsline {captionX}{ {Auzug 25}\hspace *{1cm}.}{68}{captionX.25}
+\contentsline {captionX}{ {Auzug 26}\hspace *{1cm}.}{69}{captionX.26}
+\contentsline {captionX}{ {Auzug 27}\hspace *{1cm}.}{70}{captionX.27}
+\contentsline {captionX}{ {Auzug 28}\hspace *{1cm}.}{70}{captionX.28}
+\contentsline {captionX}{ {Auzug 29}\hspace *{1cm}.}{70}{captionX.29}
+\contentsline {captionX}{ {Auzug 30}\hspace *{1cm}.}{70}{captionX.30}
+\contentsline {captionX}{ {Auzug 31}\hspace *{1cm}.}{71}{captionX.31}
+\contentsline {captionX}{ {Auzug 32}\hspace *{1cm}.}{72}{captionX.32}
+\contentsline {captionX}{ {Auzug 33}\hspace *{1cm}.}{73}{captionX.33}
+\contentsline {captionX}{ {Auzug 34}\hspace *{1cm}Gestik in Designmeetings.}{94}{captionX.34}
+\contentsline {captionX}{ {Auzug 35}\hspace *{1cm}Arbeiten an digitalen Tabletop-Systemen.}{99}{captionX.35}
