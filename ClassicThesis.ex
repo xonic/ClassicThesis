a2600eb09c9d02cb5fc1344695828c292de61eec
Thomas Nägele <xonic@gmx.eu>
2011-02-20 17:33:29 +0100

    conclude chapter 4 collab design

--- ClassicThesis.ex
+++ ClassicThesis.ex
@@ -1,35 +1,35 @@
-\contentsline {captionX}{ {Auzug 1}\hspace *{1cm}Vorschlag von Kerry.}{55}{captionX.1}
-\contentsline {captionX}{ {Auzug 2}\hspace *{1cm}Johns Sicherstellung.}{55}{captionX.2}
-\contentsline {captionX}{ {Auzug 3}\hspace *{1cm}Kerrys Hinweis.}{56}{captionX.3}
-\contentsline {captionX}{ {Auzug 4}\hspace *{1cm}Kerrys Anliegen wird ignoriert.}{56}{captionX.4}
-\contentsline {captionX}{ {Auzug 5}\hspace *{1cm}Kerry hat Erfolg.}{56}{captionX.5}
-\contentsline {captionX}{ {Auzug 6}\hspace *{1cm}Johns Vorschlag.}{57}{captionX.6}
-\contentsline {captionX}{ {Auzug 7}\hspace *{1cm}Kerry best\IeC {\"a}tigt Ivan.}{57}{captionX.7}
-\contentsline {captionX}{ {Auzug 8}\hspace *{1cm}.}{58}{captionX.8}
-\contentsline {captionX}{ {Auzug 9}\hspace *{1cm}.}{59}{captionX.9}
-\contentsline {captionX}{ {Auzug 10}\hspace *{1cm}.}{59}{captionX.10}
-\contentsline {captionX}{ {Auzug 11}\hspace *{1cm}.}{60}{captionX.11}
-\contentsline {captionX}{ {Auzug 12}\hspace *{1cm}.}{60}{captionX.12}
-\contentsline {captionX}{ {Auzug 13}\hspace *{1cm}.}{60}{captionX.13}
-\contentsline {captionX}{ {Auzug 14}\hspace *{1cm}.}{61}{captionX.14}
-\contentsline {captionX}{ {Auzug 15}\hspace *{1cm}.}{61}{captionX.15}
-\contentsline {captionX}{ {Auzug 16}\hspace *{1cm}.}{61}{captionX.16}
-\contentsline {captionX}{ {Auzug 17}\hspace *{1cm}.}{62}{captionX.17}
-\contentsline {captionX}{ {Auzug 18}\hspace *{1cm}.}{62}{captionX.18}
-\contentsline {captionX}{ {Auzug 19}\hspace *{1cm}.}{63}{captionX.19}
-\contentsline {captionX}{ {Auzug 20}\hspace *{1cm}.}{64}{captionX.20}
-\contentsline {captionX}{ {Auzug 21}\hspace *{1cm}.}{64}{captionX.21}
-\contentsline {captionX}{ {Auzug 22}\hspace *{1cm}.}{65}{captionX.22}
-\contentsline {captionX}{ {Auzug 23}\hspace *{1cm}.}{66}{captionX.23}
-\contentsline {captionX}{ {Auzug 24}\hspace *{1cm}.}{67}{captionX.24}
-\contentsline {captionX}{ {Auzug 25}\hspace *{1cm}.}{68}{captionX.25}
-\contentsline {captionX}{ {Auzug 26}\hspace *{1cm}.}{69}{captionX.26}
-\contentsline {captionX}{ {Auzug 27}\hspace *{1cm}.}{70}{captionX.27}
-\contentsline {captionX}{ {Auzug 28}\hspace *{1cm}.}{70}{captionX.28}
-\contentsline {captionX}{ {Auzug 29}\hspace *{1cm}.}{70}{captionX.29}
-\contentsline {captionX}{ {Auzug 30}\hspace *{1cm}.}{70}{captionX.30}
-\contentsline {captionX}{ {Auzug 31}\hspace *{1cm}.}{71}{captionX.31}
-\contentsline {captionX}{ {Auzug 32}\hspace *{1cm}.}{72}{captionX.32}
-\contentsline {captionX}{ {Auzug 33}\hspace *{1cm}.}{73}{captionX.33}
+\contentsline {captionX}{ {Auzug 1}\hspace *{1cm}Vorschlag von Kerry}{55}{captionX.1}
+\contentsline {captionX}{ {Auzug 2}\hspace *{1cm}John will die Problematik kl\IeC {\"a}ren}{55}{captionX.2}
+\contentsline {captionX}{ {Auzug 3}\hspace *{1cm}Ein Hinweis}{55}{captionX.3}
+\contentsline {captionX}{ {Auzug 4}\hspace *{1cm}Das Anliegen wird ignoriert}{56}{captionX.4}
+\contentsline {captionX}{ {Auzug 5}\hspace *{1cm}Kerry wird erh\IeC {\"o}rt}{56}{captionX.5}
+\contentsline {captionX}{ {Auzug 6}\hspace *{1cm}Der zeitliche Ablauf}{56}{captionX.6}
+\contentsline {captionX}{ {Auzug 7}\hspace *{1cm}Best\IeC {\"a}tigung f\IeC {\"u}r Ivan}{57}{captionX.7}
+\contentsline {captionX}{ {Auzug 8}\hspace *{1cm}Planung zuerst}{58}{captionX.8}
+\contentsline {captionX}{ {Auzug 9}\hspace *{1cm}Es ist bereits sp\IeC {\"a}t}{59}{captionX.9}
+\contentsline {captionX}{ {Auzug 10}\hspace *{1cm}Zeitdruck}{59}{captionX.10}
+\contentsline {captionX}{ {Auzug 11}\hspace *{1cm}Sollen wir bereits voranschreiten?}{59}{captionX.11}
+\contentsline {captionX}{ {Auzug 12}\hspace *{1cm}Nochmals alles abkl\IeC {\"a}ren}{60}{captionX.12}
+\contentsline {captionX}{ {Auzug 13}\hspace *{1cm}Der Prototyp}{60}{captionX.13}
+\contentsline {captionX}{ {Auzug 14}\hspace *{1cm}Kerry teilt ihre Vorstellung mit}{60}{captionX.14}
+\contentsline {captionX}{ {Auzug 15}\hspace *{1cm}Ma\IeC {\ss } nehmen}{61}{captionX.15}
+\contentsline {captionX}{ {Auzug 16}\hspace *{1cm}Die Rechtfertigung}{61}{captionX.16}
+\contentsline {captionX}{ {Auzug 17}\hspace *{1cm}Unterbrechung}{62}{captionX.17}
+\contentsline {captionX}{ {Auzug 18}\hspace *{1cm}Wieder vereint}{62}{captionX.18}
+\contentsline {captionX}{ {Auzug 19}\hspace *{1cm}Inspektion}{63}{captionX.19}
+\contentsline {captionX}{ {Auzug 20}\hspace *{1cm}Wieder zur\IeC {\"u}ck}{63}{captionX.20}
+\contentsline {captionX}{ {Auzug 21}\hspace *{1cm}Der Zielpreis}{64}{captionX.21}
+\contentsline {captionX}{ {Auzug 22}\hspace *{1cm}Der Report}{64}{captionX.22}
+\contentsline {captionX}{ {Auzug 23}\hspace *{1cm}Missverst\IeC {\"a}ndnisse}{65}{captionX.23}
+\contentsline {captionX}{ {Auzug 24}\hspace *{1cm}Bereits vergessen}{66}{captionX.24}
+\contentsline {captionX}{ {Auzug 25}\hspace *{1cm}Was erwartet der Benutzer?}{67}{captionX.25}
+\contentsline {captionX}{ {Auzug 26}\hspace *{1cm}Ausformulieren}{68}{captionX.26}
+\contentsline {captionX}{ {Auzug 27}\hspace *{1cm}Eine Idee}{69}{captionX.27}
+\contentsline {captionX}{ {Auzug 28}\hspace *{1cm}Beharren}{69}{captionX.28}
+\contentsline {captionX}{ {Auzug 29}\hspace *{1cm}Emotionale Bindung}{69}{captionX.29}
+\contentsline {captionX}{ {Auzug 30}\hspace *{1cm}Auflockerung}{69}{captionX.30}
+\contentsline {captionX}{ {Auzug 31}\hspace *{1cm}Dr\IeC {\"a}ngen}{70}{captionX.31}
+\contentsline {captionX}{ {Auzug 32}\hspace *{1cm}Uneinigkeit}{71}{captionX.32}
+\contentsline {captionX}{ {Auzug 33}\hspace *{1cm}Zwei gegen Einen}{72}{captionX.33}
 \contentsline {captionX}{ {Auzug 34}\hspace *{1cm}Gestik in Designmeetings.}{94}{captionX.34}
 \contentsline {captionX}{ {Auzug 35}\hspace *{1cm}Arbeiten an digitalen Tabletop-Systemen.}{99}{captionX.35}
