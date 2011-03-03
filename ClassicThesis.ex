57bd426abdafbe3529a9beacbb770f2d47765c3e
Thomas Nägele <xonic@gmx.eu>
2011-03-03 14:58:43 +0100

    fix extract column length

--- ClassicThesis.ex
+++ ClassicThesis.ex
@@ -1,49 +1,49 @@
-\contentsline {captionX}{ {Auszug 1}\hspace *{1cm}Vorschlag von Kerry}{90}{captionX.1}
-\contentsline {captionX}{ {Auszug 2}\hspace *{1cm}John will die Problematik kl\IeC {\"a}ren}{90}{captionX.2}
-\contentsline {captionX}{ {Auszug 3}\hspace *{1cm}Ein Hinweis}{91}{captionX.3}
-\contentsline {captionX}{ {Auszug 4}\hspace *{1cm}Das Anliegen wird ignoriert}{91}{captionX.4}
-\contentsline {captionX}{ {Auszug 5}\hspace *{1cm}Kerry wird erh\IeC {\"o}rt}{91}{captionX.5}
-\contentsline {captionX}{ {Auszug 6}\hspace *{1cm}Der zeitliche Ablauf}{92}{captionX.6}
-\contentsline {captionX}{ {Auszug 7}\hspace *{1cm}Best\IeC {\"a}tigung f\IeC {\"u}r Ivan}{92}{captionX.7}
-\contentsline {captionX}{ {Auszug 8}\hspace *{1cm}Planung zuerst}{93}{captionX.8}
-\contentsline {captionX}{ {Auszug 9}\hspace *{1cm}Es ist bereits sp\IeC {\"a}t}{94}{captionX.9}
-\contentsline {captionX}{ {Auszug 10}\hspace *{1cm}Zeitdruck}{94}{captionX.10}
-\contentsline {captionX}{ {Auszug 11}\hspace *{1cm}Sollen wir bereits voranschreiten?}{94}{captionX.11}
-\contentsline {captionX}{ {Auszug 12}\hspace *{1cm}Nochmals alles abkl\IeC {\"a}ren}{95}{captionX.12}
-\contentsline {captionX}{ {Auszug 13}\hspace *{1cm}Der Prototyp}{95}{captionX.13}
-\contentsline {captionX}{ {Auszug 14}\hspace *{1cm}Kerry teilt ihre Vorstellung mit}{95}{captionX.14}
-\contentsline {captionX}{ {Auszug 15}\hspace *{1cm}Ma\IeC {\ss } nehmen}{96}{captionX.15}
-\contentsline {captionX}{ {Auszug 16}\hspace *{1cm}Die Rechtfertigung}{96}{captionX.16}
-\contentsline {captionX}{ {Auszug 17}\hspace *{1cm}Unterbrechung}{96}{captionX.17}
-\contentsline {captionX}{ {Auszug 18}\hspace *{1cm}Wieder vereint}{97}{captionX.18}
-\contentsline {captionX}{ {Auszug 19}\hspace *{1cm}Inspektion}{98}{captionX.19}
-\contentsline {captionX}{ {Auszug 20}\hspace *{1cm}Wieder zur\IeC {\"u}ck}{98}{captionX.20}
-\contentsline {captionX}{ {Auszug 21}\hspace *{1cm}Der Zielpreis}{99}{captionX.21}
-\contentsline {captionX}{ {Auszug 22}\hspace *{1cm}Der Report}{99}{captionX.22}
-\contentsline {captionX}{ {Auszug 23}\hspace *{1cm}Missverst\IeC {\"a}ndnisse}{100}{captionX.23}
-\contentsline {captionX}{ {Auszug 24}\hspace *{1cm}Bereits vergessen}{101}{captionX.24}
-\contentsline {captionX}{ {Auszug 25}\hspace *{1cm}Was erwartet der Benutzer?}{102}{captionX.25}
-\contentsline {captionX}{ {Auszug 26}\hspace *{1cm}Ausformulieren}{103}{captionX.26}
-\contentsline {captionX}{ {Auszug 27}\hspace *{1cm}Eine Idee}{103}{captionX.27}
-\contentsline {captionX}{ {Auszug 28}\hspace *{1cm}Beharren}{104}{captionX.28}
-\contentsline {captionX}{ {Auszug 29}\hspace *{1cm}Emotionale Bindung}{104}{captionX.29}
-\contentsline {captionX}{ {Auszug 30}\hspace *{1cm}Auflockerung}{104}{captionX.30}
-\contentsline {captionX}{ {Auszug 31}\hspace *{1cm}Dr\IeC {\"a}ngen}{104}{captionX.31}
-\contentsline {captionX}{ {Auszug 32}\hspace *{1cm}Uneinigkeit}{105}{captionX.32}
-\contentsline {captionX}{ {Auszug 33}\hspace *{1cm}Zwei gegen Einen}{106}{captionX.33}
-\contentsline {captionX}{ {Auszug 34}\hspace *{1cm}Gestik in Designmeetings.}{126}{captionX.34}
-\contentsline {captionX}{ {Auszug 35}\hspace *{1cm}Arbeiten an digitalen Tabletop-Systemen.}{131}{captionX.35}
-\contentsline {captionX}{ {Auszug 36}\hspace *{1cm}Traumsituation}{173}{captionX.36}
-\contentsline {captionX}{ {Auszug 37}\hspace *{1cm}Einsatz im Unterricht}{173}{captionX.37}
-\contentsline {captionX}{ {Auszug 38}\hspace *{1cm}Farben}{174}{captionX.38}
-\contentsline {captionX}{ {Auszug 39}\hspace *{1cm}Zugeh\IeC {\"o}rigkeit}{175}{captionX.39}
-\contentsline {captionX}{ {Auszug 40}\hspace *{1cm}Kommunikation}{175}{captionX.40}
-\contentsline {captionX}{ {Auszug 41}\hspace *{1cm}Graphic-Recording}{176}{captionX.41}
-\contentsline {captionX}{ {Auszug 42}\hspace *{1cm}Fehlende Funktionalit\IeC {\"a}t}{178}{captionX.42}
-\contentsline {captionX}{ {Auszug 43}\hspace *{1cm}Transport und Kosten}{180}{captionX.43}
-\contentsline {captionX}{ {Auszug 44}\hspace *{1cm}Nutzen}{180}{captionX.44}
-\contentsline {captionX}{ {Auszug 45}\hspace *{1cm}Nutzen}{180}{captionX.45}
-\contentsline {captionX}{ {Auszug 46}\hspace *{1cm}Radieren}{181}{captionX.46}
-\contentsline {captionX}{ {Auszug 47}\hspace *{1cm}Verschieben}{182}{captionX.47}
-\contentsline {captionX}{ {Auszug 48}\hspace *{1cm}Trackpad}{182}{captionX.48}
-\contentsline {captionX}{ {Auszug 49}\hspace *{1cm}Inaktive Fenster}{183}{captionX.49}
+\contentsline {captionX}{ {Auszug 1}\hspace *{1cm}Vorschlag von Kerry}{88}{captionX.1}
+\contentsline {captionX}{ {Auszug 2}\hspace *{1cm}John will die Problematik kl\IeC {\"a}ren}{88}{captionX.2}
+\contentsline {captionX}{ {Auszug 3}\hspace *{1cm}Ein Hinweis}{88}{captionX.3}
+\contentsline {captionX}{ {Auszug 4}\hspace *{1cm}Das Anliegen wird ignoriert}{89}{captionX.4}
+\contentsline {captionX}{ {Auszug 5}\hspace *{1cm}Kerry wird erh\IeC {\"o}rt}{89}{captionX.5}
+\contentsline {captionX}{ {Auszug 6}\hspace *{1cm}Der zeitliche Ablauf}{89}{captionX.6}
+\contentsline {captionX}{ {Auszug 7}\hspace *{1cm}Best\IeC {\"a}tigung f\IeC {\"u}r Ivan}{90}{captionX.7}
+\contentsline {captionX}{ {Auszug 8}\hspace *{1cm}Planung zuerst}{91}{captionX.8}
+\contentsline {captionX}{ {Auszug 9}\hspace *{1cm}Es ist bereits sp\IeC {\"a}t}{92}{captionX.9}
+\contentsline {captionX}{ {Auszug 10}\hspace *{1cm}Zeitdruck}{92}{captionX.10}
+\contentsline {captionX}{ {Auszug 11}\hspace *{1cm}Sollen wir bereits voranschreiten?}{92}{captionX.11}
+\contentsline {captionX}{ {Auszug 12}\hspace *{1cm}Nochmals alles abkl\IeC {\"a}ren}{93}{captionX.12}
+\contentsline {captionX}{ {Auszug 13}\hspace *{1cm}Der Prototyp}{93}{captionX.13}
+\contentsline {captionX}{ {Auszug 14}\hspace *{1cm}Kerry teilt ihre Vorstellung mit}{93}{captionX.14}
+\contentsline {captionX}{ {Auszug 15}\hspace *{1cm}Ma\IeC {\ss } nehmen}{94}{captionX.15}
+\contentsline {captionX}{ {Auszug 16}\hspace *{1cm}Die Rechtfertigung}{94}{captionX.16}
+\contentsline {captionX}{ {Auszug 17}\hspace *{1cm}Unterbrechung}{94}{captionX.17}
+\contentsline {captionX}{ {Auszug 18}\hspace *{1cm}Wieder vereint}{95}{captionX.18}
+\contentsline {captionX}{ {Auszug 19}\hspace *{1cm}Inspektion}{96}{captionX.19}
+\contentsline {captionX}{ {Auszug 20}\hspace *{1cm}Wieder zur\IeC {\"u}ck}{96}{captionX.20}
+\contentsline {captionX}{ {Auszug 21}\hspace *{1cm}Der Zielpreis}{97}{captionX.21}
+\contentsline {captionX}{ {Auszug 22}\hspace *{1cm}Der Report}{97}{captionX.22}
+\contentsline {captionX}{ {Auszug 23}\hspace *{1cm}Missverst\IeC {\"a}ndnisse}{98}{captionX.23}
+\contentsline {captionX}{ {Auszug 24}\hspace *{1cm}Bereits vergessen}{99}{captionX.24}
+\contentsline {captionX}{ {Auszug 25}\hspace *{1cm}Was erwartet der Benutzer?}{100}{captionX.25}
+\contentsline {captionX}{ {Auszug 26}\hspace *{1cm}Ausformulieren}{101}{captionX.26}
+\contentsline {captionX}{ {Auszug 27}\hspace *{1cm}Eine Idee}{101}{captionX.27}
+\contentsline {captionX}{ {Auszug 28}\hspace *{1cm}Beharren}{101}{captionX.28}
+\contentsline {captionX}{ {Auszug 29}\hspace *{1cm}Emotionale Bindung}{102}{captionX.29}
+\contentsline {captionX}{ {Auszug 30}\hspace *{1cm}Auflockerung}{102}{captionX.30}
+\contentsline {captionX}{ {Auszug 31}\hspace *{1cm}Dr\IeC {\"a}ngen}{102}{captionX.31}
+\contentsline {captionX}{ {Auszug 32}\hspace *{1cm}Uneinigkeit}{103}{captionX.32}
+\contentsline {captionX}{ {Auszug 33}\hspace *{1cm}Zwei gegen Einen}{104}{captionX.33}
+\contentsline {captionX}{ {Auszug 34}\hspace *{1cm}Gestik in Designmeetings.}{124}{captionX.34}
+\contentsline {captionX}{ {Auszug 35}\hspace *{1cm}Arbeiten an digitalen Tabletop-Systemen.}{129}{captionX.35}
+\contentsline {captionX}{ {Auszug 36}\hspace *{1cm}Traumsituation}{171}{captionX.36}
+\contentsline {captionX}{ {Auszug 37}\hspace *{1cm}Einsatz im Unterricht}{171}{captionX.37}
+\contentsline {captionX}{ {Auszug 38}\hspace *{1cm}Farben}{172}{captionX.38}
+\contentsline {captionX}{ {Auszug 39}\hspace *{1cm}Zugeh\IeC {\"o}rigkeit}{173}{captionX.39}
+\contentsline {captionX}{ {Auszug 40}\hspace *{1cm}Kommunikation}{173}{captionX.40}
+\contentsline {captionX}{ {Auszug 41}\hspace *{1cm}Graphic-Recording}{174}{captionX.41}
+\contentsline {captionX}{ {Auszug 42}\hspace *{1cm}Fehlende Funktionalit\IeC {\"a}t}{176}{captionX.42}
+\contentsline {captionX}{ {Auszug 43}\hspace *{1cm}Transport und Kosten}{178}{captionX.43}
+\contentsline {captionX}{ {Auszug 44}\hspace *{1cm}Nutzen}{178}{captionX.44}
+\contentsline {captionX}{ {Auszug 45}\hspace *{1cm}Nutzen}{178}{captionX.45}
+\contentsline {captionX}{ {Auszug 46}\hspace *{1cm}Radieren}{179}{captionX.46}
+\contentsline {captionX}{ {Auszug 47}\hspace *{1cm}Verschieben}{180}{captionX.47}
+\contentsline {captionX}{ {Auszug 48}\hspace *{1cm}Trackpad}{180}{captionX.48}
+\contentsline {captionX}{ {Auszug 49}\hspace *{1cm}Inaktive Fenster}{181}{captionX.49}
